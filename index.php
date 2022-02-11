42003b176d7b837ed5698cfa7deab6a6f70cb933
Lavi Sidana <lavisidana007@gmail.com>
2022-02-11 12:19:54 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo $_SERVER['REQUEST_URI'];
+
+$routes = ["/" => __DIR__ . '/views/index.view.php', "/contact-us" => __DIR__ . '/views/contact.view.php', "/about-us" => __DIR__ . '/views/about.view.php',];
+
+$requestUri = $_SERVER['REQUEST_URI'];
+
+if(isset($routes[$requestUri])){
+    require $routes[$requestUri];
+} else{
+    echo "no route found";
+}
